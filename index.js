ffe96f92f0f2887ef70fe72a4c984d83f013c2fc
Abdikhafar <abdikhafarissack@gmail.com>
2024-07-04 13:35:59 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const lowerCaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseName);
+  }
+  function fuzzyMatch(drivers, letters) {
+    const lowerCaseLetters = letters.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseLetters));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
